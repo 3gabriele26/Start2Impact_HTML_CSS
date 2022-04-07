49f739b736e30e996becc9452ef63763fe2b7be6
3gabriele26 <gabrielegiustozzi@protonmail.com>
2022-04-07 18:39:27 +0200

    bug fixes

---
+++ assets/js/contattaci.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_yc5k1b6';
+   const templateID = 'template_nb7vsxg';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
